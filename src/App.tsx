9191b592d66908715e6bdcb32e53f59abe188d31
U Naveen Babu <136116172+naveen0913@users.noreply.github.com>
2024-12-26 18:43:41 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from './pages/SignUp/Signup.tsx';
+import Home from './pages/Home/Home.tsx';
 
-function App(){
-  
-    return (
-      <div className="App">   
-      <Signup/>
-      
-      </div>
-    );
- 
-}
+const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  // Check for token in local storage
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    setIsAuthenticated(!!token); // If token exists, set isAuthenticated to true
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={isAuthenticated ? <Navigate to="/home" /> : <Navigate to="/signin" />}
+        />
+        <Route path="/signin" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
+
