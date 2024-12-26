01daf4e5a458ba75a8bf4d92943f04533da306c3
naveen0913 <udayagirinaveen135@gmail.com>
2024-12-27 01:25:00 +0530

    Updated

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../Home/Home.css'
 import { useNavigate } from "react-router-dom";
+import * as Constants from "../../constants/constants.tsx";
+
+interface Notes{
+    uuid:string,
+    notes:string
+}
 
 function HomePage() {
-    const navigate = useNavigate(); // For 
+    const navigate = useNavigate();
+    const [notes, setNotes] = useState<Notes[]>([]);
+
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
-
-    const notes = ["Note 1", "Note 2"]; // Example 
     const [formData, setFormData] = useState({
         notes: ""
     });
+
+    useEffect(() => {
+        getNotesData(`${Constants.baseUrl}notes/list`);
+      }, []);
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setFormData({
@@ -23,13 +33,24 @@ function HomePage() {
         navigate('/signin')
     };
 
-
-
     const createNote = async (e: React.FormEvent<HTMLButtonElement>) => {
         e.preventDefault();
         if (!formData.notes) {
-            alert("Add data first");
+            alert("Add Notes data first");
             return;
+        }
+
+        const notesPayload = {
+            notes:formData.notes
+        }
+
+        const result= await postNotesData(`${Constants.baseUrl}notes/create`,notesPayload);
+        console.log(result.data);
+        if (result && result.code==="201") {
+            alert("Notes Added Successfully");
+            setNotes(result.data);
+        }else{
+            alert("Failed to add the Notes");
         }
     }
 
@@ -41,7 +62,65 @@ function HomePage() {
 
     }
 
+    async function deleteTodo(uuid:string) {        
+        try {
+            setLoading(true);
+            setError(null);
+            const token = localStorage.getItem("authToken");
+            const response = await fetch(`${Constants.baseUrl}notes/delete/${uuid}`, {
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+              },
+            });
+      
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+      
+            const result = await response.json();
+            if (result && result.code==="200") {
+                alert("Deleted Successfully")
+            }
+            
+          } catch (err) {
+            setError((err as Error).message);
+          } finally {
+            setLoading(false);
+          }
+    }
 
+    // get user notes list data
+    const getNotesData = async (url: string): Promise<void> => {
+        try {
+          setLoading(true);
+          setError(null);
+          const token = localStorage.getItem("authToken");
+          const response = await fetch(url, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`
+            },
+          });
+    
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+    
+          const result = await response.json();
+          if (result && result.code==="200") {
+            setNotes(result.data);
+          }
+          
+        } catch (err) {
+          setError((err as Error).message);
+        } finally {
+          setLoading(false);
+        }
+      };
+    
 
     //notes API Call
     const postNotesData = async (url: string, body: Record<string, any>): Promise<any> => {
@@ -91,8 +170,8 @@ function HomePage() {
                 <div className="input-group">
                     <label htmlFor="notes">Add Notes</label>
                     <textarea name="notes" value={formData.notes}
-                        onChange={handleChange}
-                        placeholder="Your Name" id="notes" rows={5} cols={50}></textarea>
+                        onChange={handleChange} required
+                        placeholder="Add your Notes" id="notes" rows={5} cols={50}></textarea>
                 </div>
 
                 <div className="btn-container">
@@ -109,13 +188,10 @@ function HomePage() {
                 <div className="notes-list">
                     {notes.map((note, index) => (
                         <div className="note-item" key={index}>
-                            <span>{note}</span>
-                            <button onClick={() => updateNote(note)}>
-                                Update
-                            </button>
+                            <span>{note.notes}</span>
                             <button
                                 className="delete-button"
-                                onClick={() => deleteNote(note)}
+                                onClick={() => deleteTodo(note.uuid)}
                             >
                                 🗑️
                             </button>
